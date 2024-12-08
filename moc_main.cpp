39bc4d1f209ebfe8975e098753370313dfed6ba2
Isaac Reynolds <isaac603@icloud.com>
2024-12-08 05:53:39 -0600

    at this point idek

--- moc_main.cpp
+++ moc_main.cpp
@@ -10,7 +10,7 @@
 #include <QtCore/qbytearray.h>
 #include <QtCore/qmetatype.h>
 #if !defined(Q_MOC_OUTPUT_REVISION)
-#error "The header file 'main.cpp' doesn't include <QObject>."
+#error "The header file 'mainwindow.h' doesn't include <QObject>."
 #elif Q_MOC_OUTPUT_REVISION != 67
 #error "This file was generated using the moc from 5.15.16. It"
 #error "cannot be used with the include files from this version of Qt."
